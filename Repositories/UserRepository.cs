1558b5a6445233f9ae9c1f00f5414899fa5961bc
N1kkigod <smirnov3.na@edu.spbstu.ru>
2021-04-30 21:08:13 +0300

    Added DeleteMail function, Added GetUser by UserName function

---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MailSystemWebApi.DataBase;
+using MailSystemWebApi.Models;
+using MailSystemWebApi.Repositories;
+
+namespace MailSystemWebApi.Repositories
+{
+    public class UserRepository<TDbModel> : IUserRepository<TDbModel> where TDbModel : User
+    {
+        private ApplicationContext Context { get; set; }
+        public UserRepository(ApplicationContext context)
+        {
+            Context = context;
+        }
+        public bool checkLogin(string login, string password)
+        {
+            try 
+            {
+                int searchUser = Context.Set<TDbModel>().Where(user => user.UserName == login).First().UserID;
+                //var selectedUser = Context.Set<TDbModel>().Select(
+                //    (user, username) => new { User = user, UserName = username }).Where(user => user.UserName == login)
+                //    .Select(user => user.UserID);
+
+                //int id = Context.Set<TDbModel>().Find
+                if (Context.Set<TDbModel>().Find(searchUser).Password == password)
+                    return true;
+                else
+                    return false;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
